c4d5a18278cec33b14fc503c17fa7640c4f200a3
CesarAHO123 <alejandroho23@hotmail.com>
2023-09-20 17:19:51 -0600

    added reactiveness to main page photo

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+  myPhoto="assets/A001.png"
+  windowSize$: Observable<Event> = fromEvent(window, 'resize');
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    // Actualiza el valor cuando cambia el tamaño de la pantalla
+    this.updatePhoto();
+  }
+  updatePhoto(): void {
+    // Aquí puedes cambiar el valor según tus necesidades
+    if (window.innerWidth < 768) {
+      this.myPhoto = "assets/A003.jpg";
+    } else {
+      this.myPhoto = "assets/A001.png";
+    }
+  }
+  ngOnInit(): void {
+    this.updatePhoto();
+  }
 
 }
